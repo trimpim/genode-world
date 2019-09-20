736ad825f3a53ba7a50f8a13f0898a010f467d71
Pirmin Duss <pirmin.duss@gapfruit.com>
2019-09-20 11:18:30 +0200

    WIP: Port of protobuf 3.9.1 and gRPC 1.24.0
    Issue #190
    
    WIP: grpc: google RPC library based on protobuf

---
+++ src/test/protobuf/add_person/target.mk
@@ -0,0 +1,20 @@
+PROTOBUF_DIR := $(call select_from_ports,protobuf_grpc)/src/lib/protobuf
+
+TARGET       := add_person
+
+LIBS         := posix protobuf stdcxx
+
+SRC_CC       += add_person.cc \
+                addressbook.pb.cc
+
+PROTOC       := /usr/local/genode/tool/current/bin/protoc
+
+CC_CXX_WARN_STRICT =
+
+vpath add_person.cc      $(PROTOBUF_DIR)/examples
+vpath addressbook.proto  $(PROTOBUF_DIR)/examples
+
+$(SRC_CC): addressbook.pb.h
+
+addressbook.pb.h: addressbook.proto
+	$(VERBOSE)$(PROTOC) --proto_path=$(PROTOBUF_DIR)/examples/ --cpp_out=. $<
---
+++ src/test/protobuf/list_people/target.mk
@@ -0,0 +1,20 @@
+PROTOBUF_DIR := $(call select_from_ports,protobuf_grpc)/src/lib/protobuf
+
+TARGET       := list_people
+
+LIBS         := posix protobuf stdcxx
+
+SRC_CC       += list_people.cc \
+                addressbook.pb.cc
+
+PROTOC       := /usr/local/genode/tool/current/bin/protoc
+
+CC_CXX_WARN_STRICT =
+
+vpath list_people.cc     $(PROTOBUF_DIR)/examples
+vpath addressbook.proto  $(PROTOBUF_DIR)/examples
+
+$(SRC_CC): addressbook.pb.h
+
+addressbook.pb.h: addressbook.proto
+	$(VERBOSE)$(PROTOC) --proto_path=$(PROTOBUF_DIR)/examples/ --cpp_out=. $<
