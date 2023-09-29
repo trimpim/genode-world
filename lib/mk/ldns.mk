600738e399e78139d838718fa0b0b266ad25309d
Pirmin Duss <pirmin.duss@gapfruit.com>
2023-09-29 11:25:44 +0200

    lib/ldns: fix build for openssl 3
    Issue #342

--- lib/mk/ldns.mk
+++ lib/mk/ldns.mk
@@ -9,5 +9,11 @@ SRC_C += $(filter-out linktest.c,$(SRC_LDNS_C))
 SRC_C += $(notdir $(wildcard $(LDNS_SRC_DIR)/compat/b64*.c))
 SRC_CC +=  getproto.cc
 
+CC_C_OPT += -DHAVE_DSA_GET0_PQG -DHAVE_DSA_SIG_GET0 -DHAVE_DSA_SIG_SET0
+CC_C_OPT += -DHAVE_EVP_MD_CTX_NEW -DHAVE_ECDSA_SIG_GET0 -DHAVE_DSA_GET0_KEY
+CC_C_OPT += -Wno-stringop-truncation -Wno-array-parameter
+CC_C_OPT += -Wno-int-conversion -Wno-stringop-overflow
+CC_C_OPT += -Wno-implicit-function-declaration
+
 vpath %.c $(LDNS_SRC_DIR) $(LDNS_SRC_DIR)/compat
 vpath %.cc $(REP_DIR)/src/lib/ldns
